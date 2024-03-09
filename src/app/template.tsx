9a12e9a46a5a60ac75669a33ef7ef7be73980539
Patrick McCallum <11825795+patrickmccallum@users.noreply.github.com>
2024-03-09 12:23:17 +1000

    chore: add HeaderLink component, GitHub link

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link'
 import { PropsWithChildren } from 'react'
+import {cn} from "@/lib/style-utils";
 
 export default function GlobalTemplate({ children }: PropsWithChildren) {
     return (
         <div className="mx-auto flex min-h-screen w-full max-w-4xl flex-col gap-6 px-6 py-4 text-neutral-800 dark:text-neutral-50">
             <header className="flex items-center gap-4 text-lg">
-                <Link href={'/'}>Home</Link>
-                <Link href={'/blog'}>Blog</Link>
-                <Link
+                <HeaderLink href={'/'}>Home</HeaderLink>
+                <HeaderLink href={'/blog'}>Blog</HeaderLink>
+                <HeaderLink
                     href={
                         'https://github.com/patrickmccallum/nextjs-blog-starter'
                     }
                     className="ml-auto"
                 >
-                    GitHub
-                </Link>
+                    GitHub Repo
+                </HeaderLink>
             </header>
             <main>{children}</main>
             <footer className="flex justify-between border-t border-neutral-200 py-4 text-sm text-neutral-500 dark:border-neutral-600">
@@ -40,5 +41,13 @@ export default function GlobalTemplate({ children }: PropsWithChildren) {
                 </ul>
             </footer>
         </div>
+    )
+}
+
+const HeaderLink = ({ href, className, children }: PropsWithChildren<{ href: string, className?: string }>) => {
+    return (
+        <Link href={href} className={cn("hover:decoration-solid underline decoration-dashed underline-offset-4", className)}>
+            {children}
+        </Link>
     )
 }
