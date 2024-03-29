a03ce45502571564ea2a3fb0115c9e7dc07c5e54
Patrick McCallum <11825795+patrickmccallum@users.noreply.github.com>
2024-03-29 14:39:34 +1000

    chore: update packages, add prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    singleQuote: true,
+    semi: false,
+    tabWidth: 4,
+    trailingComma: 'all',
+    bracketSameLine: false,
+    htmlWhitespaceSensitivity: 'css',
+    proseWrap: 'always',
+    printWidth: 80,
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleAttributePerLine: true,
+}
